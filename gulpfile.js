974f34c1f7556127f26fdfd68bfc1e8407262daf
sethdorris <sethc.dorris@gmail.com>
2016-04-08 16:52:08 -0600

    restructured project directory, changed gulpfile to handle serverside es2015 compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const react = require('gulp-react');
+const htmlreplace = require('gulp-html-replace');
+
+const path = {
+    HTML: './index.html',
+    ALL: ['']
+}
 
 gulp.task('default', () => {
-    return gulp.src('./app/server.js')
+    return gulp.src('./server/server.js')
                 .pipe(babel({
                     presets: ['es2015']
                 }))
-                .pipe(gulp.dest("/"));
+                .pipe(gulp.dest(__dirname + "server/build"));
 });
