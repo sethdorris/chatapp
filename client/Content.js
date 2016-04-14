c229b36ee4e54af49d64e49b4d764891d1b4f924
sethdorris <sethc.dorris@gmail.com>
2016-04-14 16:55:01 -0600

    Created content component

---
+++ client/Content.js
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+class Content extends React.Component {
+    render() {
+        return (
+                <div className="col-md-8">
+                    Content
+                </div>
+            )
+    }
+}
+export default Content;
