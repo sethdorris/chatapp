9318f55bb199825995999092100399bfa7cdafe7
sethdorris <sethc.dorris@gmail.com>
2016-04-14 16:55:24 -0600

    created menu component

---
+++ client/Menu.js
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+class Menu extends React.Component {
+    render() {
+        return (
+                <div className="col-md-4">
+                    Menu
+                </div>
+            )
+    }
+}
+export default Menu;
