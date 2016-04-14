e53cf3397b91cd545ae724643030d1fe1934b03b
sethdorris <sethc.dorris@gmail.com>
2016-04-14 16:55:39 -0600

    Created main component to render

---
+++ client/chatcontainer.js
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import Menu from './Menu';
+import Content from './Content';
+class ChatContainer extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <Menu />
+                <Content />
+            </div>
+            )
+    }
+};
+export default ChatContainer;
