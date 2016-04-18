d8156e8486d096e9ae98707aad553b8aaaa4bbb7
sethdorris <sethc.dorris@gmail.com>
2016-04-18 14:35:52 -0600

    Created a message component

---
+++ client/Message.js
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+
+class Message extends React.Component {
+    constructor(props) {
+        super();
+    }
+
+    render() {
+        return (
+                <li>{this.props.content}</li>
+            )
+    }
+}
+
+export default Message;
