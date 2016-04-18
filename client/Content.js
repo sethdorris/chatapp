1f6a0f3cdd209368f63036c59c1ca5f62859a3a0
sethdorris <sethc.dorris@gmail.com>
2016-04-18 14:34:52 -0600

    Added a set state and moved send function here

--- client/Content.js
+++ client/Content.js
@@ -5,12 +5,23 @@ import ChatWindow from './ChatWindow';
 class Content extends React.Component {
     constructor(props) {
         super();
+
+        this.state = {
+            messages: [{
+                title: "test",
+                content: "this is my content"
+            }]
+        }
+    }
+
+    send () {
+        console.log("Firing");
     }
     render() {
         return (
                 <div className="col-md-8 col-xs-8 text-center content">
-                    <ChatWindow />
-                    <Controls send={this.props.send} />
+                    <ChatWindow messages={this.state.messages}/>
+                    <Controls send={this.send}/>
                 </div>
             )
     }
