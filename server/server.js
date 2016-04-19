57e056e5219e10d76737c1ac7ba723f25e32cfb1
sethdorris <sethc.dorris@gmail.com>
2016-04-19 10:53:17 -0600

    Updated server file to include websocket

--- server/server.js
+++ server/server.js
@@ -1,13 +1,34 @@
 import express from "express";
-const app = express();
+import url from 'url';
+const WebSocketServer = require('ws').Server;
 const http = require('http').Server(app);
+const wss = new WebSocketServer({server: http});
+const app = express();
+
+
+app.use((req, res) => {
+    res.send({msg: "hello"})
+});
 
 app.get('/', (req, res) => {
     res.sendFile("index.html");
 });
 
+wss.on('connection', function connection(ws) {
+    let location = url.parse(ws.upgradeReq.url, true)
+
+    console.log(location);
+
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
+
+    ws.send('something');
+})
+
+http.on('request', app);
 http.listen(3000, () => {
-    console.log("Wired");
+    console.log("Listening on " + http.address().port);
 });
 
 console.log("Wired")
