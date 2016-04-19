38b5bf416386d91b7eaec8827f1e89eab5991691
sethdorris <sethc.dorris@gmail.com>
2016-04-19 10:51:07 -0600

    Testing clientside websocket interaction

--- client/app.js
+++ client/app.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ChatContainer from './chatcontainer';
+/*const ws = new WebSocket('ws://localhost:3000');
+ws.onopen = () => {
+    ws.send("Thanks!");
+}*/
 
 ReactDOM.render(<ChatContainer />, document.getElementById('app'));
