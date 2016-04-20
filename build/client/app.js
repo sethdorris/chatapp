17dbe098cb49b5c09adca5a6511323c4721dce77
sethdorris <sethc.dorris@gmail.com>
2016-04-20 10:16:34 -0600

    Refactored menu to stateless component

--- build/client/app.js
+++ build/client/app.js
@@ -180,42 +180,19 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 var _react = require("react");
 
 var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var Menu = function (_React$Component) {
-    _inherits(Menu, _React$Component);
-
-    function Menu() {
-        _classCallCheck(this, Menu);
-
-        return _possibleConstructorReturn(this, Object.getPrototypeOf(Menu).apply(this, arguments));
-    }
-
-    _createClass(Menu, [{
-        key: "render",
-        value: function render() {
-            return _react2.default.createElement(
-                "div",
-                { className: "col-md-4 col-xs-4 text-center menu" },
-                "Menu"
-            );
-        }
-    }]);
-
-    return Menu;
-}(_react2.default.Component);
+var Menu = function Menu(props) {
+    return _react2.default.createElement(
+        "div",
+        { className: "col-md-4 col-xs-4 text-center menu" },
+        "Menu"
+    );
+};
 
 exports.default = Menu;
 
--- client/Menu.js
+++ client/Menu.js
@@ -1,11 +1,11 @@
 ﻿import React from 'react';
-class Menu extends React.Component {
-    render() {
-        return (
-                <div className="col-md-4 col-xs-4 text-center menu">
-                    Menu
-                </div>
-            )
+
+const Menu = (props) => {
+    return (
+        <div className="col-md-4 col-xs-4 text-center menu">
+            Menu
+        </div>
+        )
     }
-}
+
 export default Menu;
