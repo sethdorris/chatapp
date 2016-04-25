3bbaca99eb3a5b2157da16342890826618acd074
sethdorris <sethc.dorris@gmail.com>
2016-04-25 14:45:07 -0600

    Created a landing page component

---
+++ client/Landing.js
@@ -0,0 +1,16 @@
+﻿import React from 'react';
+
+const Landing = (props) => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-sm-12 col-md-12">
+                    <input className="form-control" type="text" placeholder="Username" />
+                    <button className="btn btn-success" type="submit">Connect</button>
+                </div>  
+            </div>
+        </div>
+        )
+                    }
+
+export default Landing;
