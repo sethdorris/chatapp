05937dfbe982e3dc040e6f82cb174353e63023d9
sethdorris <sethc.dorris@gmail.com>
2016-04-27 10:41:07 -0600

    refactored Landing page to a stateful component in order to pass username entered

--- client/Landing.js
+++ client/Landing.js
@@ -1,20 +1,31 @@
 ﻿import React from 'react';
 import {Link} from 'react-router';
 
-const Landing = (props) => {
-    return (
-        <div className="container landing">
-            <div className="row">
-                <div className="col-sm-4 col-md-4 col-offset-4">
-                    <label htmlFor="username">Username: </label>
-                    <input className="form-control" type="text" placeholder="Username" />
-                    <Link to="/main">
-                        <button className="btn btn-success text-center" type="submit">Connect</button>
-                    </Link>
-                </div>  
+class Landing extends React.Component {
+    constructor(props) {
+        super();
+    }
+
+    connectButton() {
+
+    }
+
+    render() {
+        return (
+            <div className="container landing">
+                <div className="row">
+                    <div className="col-sm-4 col-md-4 col-offset-4">
+                        <label htmlFor="username">Username: </label>
+                        <input className="form-control" type="text" placeholder="Username" />
+                        <Link to="/main">
+                            <button className="btn btn-success text-center" type="submit" onclick={this.connectButton}>Connect</button>
+                        </Link>
+                    </div>  
+                </div>
             </div>
-        </div>
-        )
+            )
+    }
+
 }
 
 export default Landing;
