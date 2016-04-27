e8f393d859d6ac027472265f59119d819ecc11a0
sethdorris <sethc.dorris@gmail.com>
2016-04-27 10:41:34 -0600

    Imported reducer and created a store

--- client/routes/index.js
+++ client/routes/index.js
@@ -4,11 +4,15 @@ import app from '../app';
 import Landing from '../Landing';
 import Main from '../chatcontainer';
 import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducer from '../reducers/index';
 
-let store = createStore();
+let store = createStore(reducer);
+
+console.log(store.getState());
 
 const routes = (
-    <Provider store={}>
+    <Provider store={store}>
         <Router history={hashHistory}>
             <Route path="/" component={Landing}>
                 <IndexRoute component={Landing} />
