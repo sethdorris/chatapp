6a8d24ec709b6140deef4eda9f7d168d0cc0f7eb
sethdorris <sethc.dorris@gmail.com>
2016-04-27 10:40:34 -0600

    Added a reducers folder and file

---
+++ client/reducers/index.js
@@ -0,0 +1,18 @@
+﻿import actions from '../actions/index';
+
+const reducer = (state, action) => {
+    if (typeof state == 'undefined') {
+        return {}
+    } 
+
+    switch (action.type) {
+        case 'SET_USERNAME':
+            return {
+                username: action.value
+            }
+        default:
+
+    }
+}
+
+export default reducer;
