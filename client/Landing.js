5d38fd54520effe0a09f51685773bfd87eda38a0
sethdorris <sethc.dorris@gmail.com>
2016-04-28 15:25:49 -0600

    Implemented socketConnectInit and socketOnOpen actions

--- client/Landing.js
+++ client/Landing.js
@@ -1,7 +1,7 @@
 ﻿import React from 'react';
 import {Link} from 'react-router';
 import {connect} from 'react-redux';
-import {setUsername, socketConnectInit} from './actions/index';
+import {setUsername, socketConnectInit, socketOnOpen} from './actions/index';
 const PropTypes = React.PropTypes;
 
 
@@ -14,15 +14,14 @@ class Landing extends React.Component {
 
     connectButton() {
         const {dispatch} = this.props;
-        let connectingString = "Connecting";
         let login = document.getElementById("usernameinput").value
-        let webSocket = new WebSocket("ws://localhost:3001");
-        console.log("WebSocket", webSocket);
-        let btn = document.getElementById("connectBtn");
+        let webSocket = new WebSocket("ws://localhost:3000");
+        
         dispatch(setUsername(login));
         dispatch(socketConnectInit(webSocket.url));
         webSocket.onopen = () => {
             console.log("websocket opened", webSocket);
+            dispatch(socketOnOpen())
             this.context.router.push({
                 pathname: '/Main'
             })
@@ -33,8 +32,8 @@ class Landing extends React.Component {
         return (
             <div className="container landing">
                 <div className="row">
-                    <div className="col-sm-4 col-md-4 col-offset-4">
-                        <label htmlFor="username">Username: </label>
+                    <div className="col-sm-offset-4 col-md-offset-4 col-sm-4 col-md-4 text-center">
+                        <h2 className="signinHeader">SIGN IN</h2>
                         <input className="form-control" type="text" id="usernameinput" placeholder="Username" />
                         <button className="btn btn-success text-center" id="connectBtn" type="submit" onClick={this.connectButton}>Connect</button>
                     </div>  
