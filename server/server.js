13c6d4e1651198f49b6c777a0d8dea6b3581448d
sethdorris <sethc.dorris@gmail.com>
2016-04-29 15:39:35 -0600

    Implemented express static for static content files

--- server/server.js
+++ server/server.js
@@ -1,34 +1,25 @@
 import express from "express";
 import url from 'url';
+import path from 'path';
 const WebSocketServer = require('ws').Server;
 const http = require('http').Server(app);
 const wss = new WebSocketServer({server: http});
 const app = express();
 
-
-app.use((req, res) => {
-    res.send({msg: "hello"})
-});
+app.use(express.static(path.resolve('../')));
 
 app.get('/', (req, res) => {
-    res.sendFile("index.html");
+    res.sendFile(path.resolve("../index.html"), {}, 
+        (err) => {
+            if (err) {
+                console.log("Error grabbing Index", err);
+            }
+        }
+    );
 });
-
-wss.on('connection', function connection(ws) {
-    let location = url.parse(ws.upgradeReq.url, true)
-
-    console.log(location);
-
-    ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
-    });
-
-    ws.send('something');
-})
 
 http.on('request', app);
 http.listen(3000, () => {
     console.log("Listening on " + http.address().port);
 });
 
-console.log("Wired")
