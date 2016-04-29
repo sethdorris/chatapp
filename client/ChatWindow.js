b6663138694488f87361c3b6813eb82459b3da91
sethdorris <sethc.dorris@gmail.com>
2016-04-29 10:36:57 -0600

    Refactored ChatWindow into a stateless component

--- client/ChatWindow.js
+++ client/ChatWindow.js
@@ -1,23 +1,16 @@
 ﻿import React from 'react';
 import Message from './Message';
 
-class ChatWindow extends React.Component {
-    constructor(props) {
-        super();
-    }
-
-    render() {
-
-        return (
-                <div className="row">
-                    <div className="col-md-12 col-xs-12 chatwindow">
-                        <ul className="messages">
+const ChatWindow = (props) => {
+    return (
+        <div className="row">
+            <div className="col-md-12 col-xs-12 chatwindow">
+                <ul className="messages">
                             
-                        </ul>
-                    </div>
-                </div>
-            )
-    }
+                </ul>
+            </div>
+        </div>
+    )
 }
 
 export default ChatWindow;
