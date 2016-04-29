3923beaf5ddf0f7715b14ac06b7c3a770799ffd0
sethdorris <sethc.dorris@gmail.com>
2016-04-29 09:06:06 -0600

    Refactored Content into a stateless component

--- client/Content.js
+++ client/Content.js
@@ -2,38 +2,12 @@
 import Controls from './Controls';
 import ChatWindow from './ChatWindow';
 
-/*
-    const ws = new WebSocket("ws://localhost:3000");
-        ws.onopen = () => {
-            ws.send("hi");
-            ws.onmessage = (message) => {
-                console.log("message: ", message.data);
-            }
-        }
-        */
-
-
-class Content extends React.Component {
-    constructor(props) {
-        super();
-
-    }
-
-    send () {
-
-    }
-
-    componentDidMount() {
-    
-    }
-
-    render() {
-        return (
-                <div className="col-md-8 col-xs-8 text-center content">
-                    <ChatWindow username={this.props.username}/>
-                    <Controls />
-                </div>
-            )
-    }
+const Content = (props) => {
+    return (
+            <div className="col-md-8 col-xs-8 text-center content">
+                <ChatWindow username={this.props.username}/>
+                <Controls />
+            </div>
+        )
 }
 export default Content;
