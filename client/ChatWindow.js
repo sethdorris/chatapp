e9b564169873e89b0604888901b799dbe804803a
sethdorris <sethc.dorris@gmail.com>
2016-05-02 15:16:10 -0600

    Mapping message state to ChatWindow props

--- client/ChatWindow.js
+++ client/ChatWindow.js
@@ -3,6 +3,8 @@ import Message from './Message';
 import {connect} from 'react-redux';
 
 const ChatWindow = (props) => {
+    const {message} = this.props;
+
     return (
         <div className="row">
             <div className="col-md-12 col-xs-12 chatwindow">
@@ -14,10 +16,10 @@ const ChatWindow = (props) => {
     )
 }
 
-const mapStateToProps = () => {
+const mapStateToProps = (state) => {
     return {
         message: state.messages
     }
 }
 
-export default connect()(ChatWindow);
+export default connect(mapStateToProps)(ChatWindow);
