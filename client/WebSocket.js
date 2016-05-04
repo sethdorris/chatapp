1ace295be3f4ac41eca4477ed0805ff88a226f7a
sethdorris <sethc.dorris@gmail.com>
2016-05-04 16:13:32 -0600

    implemented async await on the sendmessage function of WebSocket.js

--- client/WebSocket.js
+++ client/WebSocket.js
@@ -2,14 +2,23 @@
 
 const server = {
     connected: false,
+    isConnected: function() {
+        if (ws.readyState == 1) {
+            this.connected = true;
+        } else {
+            this.connected = false;
+        }
+        return this.connected;
+    },
     connect: function() {
         if (ws.readyState == 1) {
             this.connected = true;
         }
         return this.connected;
     },
-    sendmessage: () => {
-        ws.send(JSON.stringify({message: "hi"}));
+    sendmessage: async function(text) {
+        await ws.send(JSON.stringify({message: text}));
+        return console.log("You sent this message: ", JSON.stringify({message: text}));
     }
 }
 
