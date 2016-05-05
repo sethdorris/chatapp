f5bfe6875babb0895ece961ea148843ed3088d27
sethdorris <sethc.dorris@gmail.com>
2016-05-05 11:01:32 -0600

    Passing username and messages to ChatWindow component from Content component

--- client/Content.js
+++ client/Content.js
@@ -5,7 +5,7 @@ import ChatWindow from './ChatWindow';
 const Content = (props) => {
     return (
             <div className="col-md-8 col-xs-8 text-center content">
-                <ChatWindow username={props.username}/>
+                <ChatWindow username={props.username} messages={props.messages}/>
                 <Controls send={props.send}/>
             </div>
         )
