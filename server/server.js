f2d50f80f35f02fe28fd2bec4ddbb472d9c6932c
sethdorris <sethc.dorris@gmail.com>
2016-05-10 13:55:52 -0600

    Deleted the use of express-ws and implemented a session variable to track users that disconnect from the application

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ import express from "express";
 import url from 'url';
 import path from 'path';
 import session from 'express-session';
-//const WebSocketServer = require('ws').Server;
-//const http = require('http').Server(app);
-//const wss = new WebSocketServer({
-//    server: http
-//});
+const WebSocketServer = require('ws').Server;
+const http = require('http').Server(app);
+const wss = new WebSocketServer({
+    server: http
+});
 const app = express();
-const expressWs = require('express-ws')(app); 
+
 app.use(express.static(path.join(__dirname, "../client"), {index: false}));
 app.use(session({
     secret: "chatappsecret",
@@ -20,6 +20,7 @@ let users = [];
 let sess;
 
 app.get('/', (req, res, next) => {
+    sess = req.session;
     res.sendFile(path.resolve("../index.html"), {}, 
         (err) => {
             if (err) {
@@ -29,53 +30,47 @@ app.get('/', (req, res, next) => {
     );
 });
 
-app.ws('/', (ws, req) => {
-    ws.on('connection', () => {
-        console.log("req", req.session)
-    })
-})
+wss.broadcast = (data) => {
+    wss.clients.forEach((client) => {
+        client.send(JSON.stringify(data));
+    });
+};
 
-//wss.broadcast = (data) => {
-//    wss.clients.forEach((client) => {
-//        client.send(JSON.stringify(data));
-//    });
-//};
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        let messageparse = JSON.parse(message);
+        console.log(messageparse);
+        switch (messageparse.type) {
+            case "USER_CONNECTED":
+                users.push({username: messageparse.username});
+                sess.username = messageparse.username;
+                console.log("session username", sess.username)
+                let messageobject =  {
+                    type: "FROMSERVER_USERCONNECTED",
+                    users: users
+                }                
+                wss.broadcast(messageobject);
+                break;
+            case "SEND_MESSAGE":
+                let message = {
+                    type: "FROMSERVER_NEWMESSAGE",
+                    content: messageparse.content,
+                    sentby: messageparse.sentby
+                }
+                wss.broadcast(message);
+                break;
+            default:
+                ws.send(JSON.stringify({Error: "Could not handle your message."}))
+        }  
+    });
 
-//wss.on('connection', (ws) => {
-//    ws.on('message', (message) => {
-//        let messageparse = JSON.parse(message);
-//        console.log(messageparse);
-//        switch (messageparse.type) {
-//            case "USER_CONNECTED":
-//                users.push({username: messageparse.username});
-//                let messageobject =  {
-//                    type: "FROMSERVER_USERCONNECTED",
-//                    users: users
-//                }                
-//                wss.broadcast(messageobject);
-//                break;
-//            case "SEND_MESSAGE":
-//                let message = {
-//                    type: "FROMSERVER_NEWMESSAGE",
-//                    content: messageparse.content,
-//                    sentby: messageparse.sentby
-//                }
-//                wss.broadcast(message);
-//                break;
-//            default:
-//                ws.send(JSON.stringify({Error: "Could not handle your message."}))
-//        }
-        
-//    })
-
-//})
-
-app.listen(3000, () => {
-    console.log("Server Running");
+    ws.on('close', () => {
+        console.log(sess.username + " has disconnected");
+    });
 });
 
-//http.on('request', app);
-//http.listen(3000, () => {
-//    console.log("Listening on " + http.address().port);
-//});
+http.on('request', app);
+http.listen(3000, () => {
+    console.log("Listening on " + http.address().port);
+});
 
